34b21b177b66c21f50f29ea7a14aae2791b4aa1c
A2ron <aaronbee1990@gmail.com>
2025-05-08 20:02:41 -0300

    started workoutcard

--- 2_brogram/src/components/WoroutCard.jsx
+++ 2_brogram/src/components/WoroutCard.jsx
@@ -1,7 +1,30 @@
-export default function WorkoutCard (){
+
+
+export default function WorkoutCard(props) {
+   const { trainingPlan, woroutIndex, type, dayNum, icon } = props;
+
+   const { warmup, workout } = trainingPlan || {};
+
    return (
-      <>
-      
-      </>
+      <div className="workout-container">
+         <div className="workout-card card">
+            <div className="plan-card-header">
+               <p>Day {dayNum}</p>
+               {icon}
+            </div>
+            <div className="plan-card-header">
+               <h2><b>{type} Workout</b></h2>
+            </div>
+         </div>
+
+         <div className="workout-div">
+            <div className="execise-name">
+               <h4>Warmup</h4>
+            </div>
+            <h6>Sets</h6>
+            <h6>Reps</h6>
+            <h6 className="weight-input">Max Weight</h6>
+         </div>
+      </div>
    )
 }
