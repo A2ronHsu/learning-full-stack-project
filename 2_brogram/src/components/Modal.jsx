3b3113fcb24f88a99120cb86fde3e4b92cbb5baf
A2ron <aaronbee1990@gmail.com>
2025-05-13 08:35:07 -0300

    finished the layout, starting styling with index.css

--- 2_brogram/src/components/Modal.jsx
+++ 2_brogram/src/components/Modal.jsx
@@ -1,9 +1,24 @@
-export default function Modal(props){
-   const {showExeciseDescription, handleCloseModal } = props;
+import ReatDom from "react-dom";
 
-   return (
-      <>
-         
-      </>
-   )
+
+export default function Modal(props){
+   const {showExerciseDescription, handleCloseModal } = props;
+   console.log(showExerciseDescription);
+   const { name, description} = showExerciseDescription
+
+   return ReatDom.createPortal((
+      <div className="modal-container">
+         <button className="modal-underlay" onClick={handleCloseModal}/>
+         <div className="modal-content">
+            <div>
+               <h6>Name</h6>
+               <h2 className="skill-name">{name.replaceAll('-',' ')}</h2>
+            </div>
+            <div>
+               <h6>Descrition</h6>
+               <p>{description}</p>
+            </div>
+         </div>
+      </div>
+   ),document.getElementById("portal"))
 }
